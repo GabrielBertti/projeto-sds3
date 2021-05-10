072d9369eb35bdf85cb7caca434fd86a25a8c39b
Gabriel Bertti <gabrielbertti58@gmail.com>
2021-05-09 22:09:45 -0300

    Security config

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Gabriel Bertti</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/GabrielBertti" target="_blank" rel="noreferrer">Gabriel Bertti</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
       Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
